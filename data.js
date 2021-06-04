be8d9d3f4d69258529a46310e04ad7a189e2a7bf
Malik Dunston <hello@malikdunston.com>
2021-06-03 21:24:31 -0400

    update

---
+++ data.js
@@ -0,0 +1,39 @@
+window.dogs = [
+	{
+		img: "./assets/img/doberman.jpg",
+		content: {
+			title: "Doberman Pinscer",
+			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+		}
+	},
+	{
+		img: "./assets/img/husky.jpg",
+		content: {
+			title: "Siberian Husky",
+			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+		}
+	},
+	{
+		img: "./assets/img/pitbull.jpg",
+		content: {
+			title: "American Pit Bull Terrier",
+			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+		}
+	},
+]
+window.cats = [
+	{
+		img: "./assets/img/siamese.jpg",
+		content: {
+			title: "Siamese",
+			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+		}
+	},
+	{
+		img: "./assets/img/bengal.jpg",
+		content: {
+			title: "Bengal",
+			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+		}
+	}
+]
