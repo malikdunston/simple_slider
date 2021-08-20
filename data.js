64fd59a349a6c1026ef29900948cf0e14a5d0228
Malik Dunston <hello@malikdunston.com>
2021-08-19 22:30:31 -0400

    factory arch...

--- data.js
+++ data.js
@@ -3,21 +3,21 @@ window.dogs = [
 		img: "./assets/img/doberman.jpg",
 		content: {
 			title: "Doberman Pinscer",
-			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+			content: "A lovely breed, indeed"
 		}
 	},
 	{
 		img: "./assets/img/husky.jpg",
 		content: {
 			title: "Siberian Husky",
-			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+			content: "A lovely breed, indeed"
 		}
 	},
 	{
 		img: "./assets/img/pitbull.jpg",
 		content: {
 			title: "American Pit Bull Terrier",
-			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+			content: "A lovely breed, indeed"
 		}
 	},
 ]
@@ -26,21 +26,21 @@ window.cats = [
 		img: "./assets/img/siamese.jpg",
 		content: {
 			title: "Siamese",
-			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
-		}
-	},
-	{
-		img: "./assets/img/bengal.jpg",
-		content: {
-			title: "Bengal",
-			content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+			content: "A lovely breed, indeed"
 		}
 	},
 	// {
-	// 	img: "./assets/img/persian.jpg",
+	// 	img: "./assets/img/bengal.jpg",
 	// 	content: {
-	// 		title: "Persian",
-	// 		content: ";lkasjdfasdfajklj;aslkdfas;dlkfja"
+	// 		title: "Bengal",
+	// 		content: "A lovely breed, indeed"
 	// 	}
-	// }
+	// },
+	{
+		img: "./assets/img/persian.jpg",
+		content: {
+			title: "Persian",
+			content: "A lovely breed, indeed"
+		}
+	}
 ]
--- slider.js
+++ slider.js
@@ -1,47 +1,124 @@
-window.sliderJSByMalikDunston = function(){
-	"use strict";
-	document.querySelectorAll("*[slider-js]").forEach(function(elem){
-		let slider = {
-			html: elem,
-			data: makeDataList(window[elem.attributes["slider-js"].value]),
-			axis: setDefault(elem, "axis", "X"),
-			direction: setDefault(elem, "direction", "forward"),
-			index: parseInt(setDefault(elem, "offset", "1")),
-			transition: setDefault(elem, "transition", "100ms"),
-			interval: setDefault(elem, "interval", "4000")
-		}
-		render(slider);
-	// once content is loaded in all the slides (images, etc)
-	// then start the animation!!!!!!!
-		animation(slider, "start");
-		window.addEventListener("resize", function(){
-			console.log("adsfasdfa");
-			set(slider);
+"use strict";
+// namespacing...
+	window.sliderJS = window.sliderJS || {};
+	sliderJS = {
+		...sliderJS,
+		modules: [] // only 1 for now...
+	}
+// initialization...
+	window.addEventListener("load", function(event){
+		sliderJS.modules = [new Module()];
+		window.addEventListener("resize", function () {
 		})
+		console.log(sliderJS);
 	});
-	function increment(slider, forBack){
-		switch(forBack){
-			case"forward":
-				if (slider.index >= slider.data.length - 1) return;
-				slider.index ++;
-				break;
-			case "backward":
-				if (slider.index <= 0) return;
-				slider.index --;
-				break;
+
+// factories...
+	function Module(){
+		this.global = {
+			anim: 0,
+			scope: window
+		},
+		this.sliders = [];
+		this.currentIndex = 0;
+		Array.from(document.querySelectorAll("*[slider-js]")).forEach(elem => {
+			let slider = new Slider(elem, this);
+			slider.render();
+			// return slider
+		})
+	}
+	function Slider(elem, Module) {
+		this.setDefault = (elem, name, def) => {
+			return elem.attributes.hasOwnProperty(name) ? elem.attributes[name].value : def
 		}
-		set(slider);
+		this.makeDataList = (arr) => {
+			if (!arr) arr = [{
+				img: "",
+				content: {
+					title: "Your Slider Has No Data!",
+					content: "Attach some data to your slider element to begin."
+				}
+			}];
+			return [arr[arr.length - 1], ...arr, arr[0]];
+		}
+		this.html = elem,
+		this.data =  this.makeDataList(Module.global.scope[elem.attributes["slider-js"].value]), // replace this w/this.slider.feed...
+		this.axis = this.setDefault(elem, "axis", "X"),
+		this.direction = this.setDefault(elem, "direction", "forward"),
+		this.index = parseInt(this.setDefault(elem, "offset", "1")),
+		this.transition = this.setDefault(elem, "transition", "100ms"),
+		this.interval = this.setDefault(elem, "interval", "4000"),
+		this.delay = this.setDefault(elem, "delay", "1"),
+		this.feed = new Feed(this) // makeDataList should run in this...
+		this.render = () => {
+			this.html.style.cssText += "position: relative; overflow: hidden; ";
+			this.html.prepend(this.feed.html);
+			// console.log(this)
+		}
 	}
-	function set(slider){
-		animation(slider, "stop");
-		slider.html.querySelector(".slider-feed").style.transition = `${slider.transition}`;
-		slider.html.querySelector(".slider-feed").style.transform = `translate${slider.axis}(${-(slider.axis == "Y" ? slider.html.clientHeight : slider.html.clientWidth) * slider.index}px)`;
-		animation(slider, "start");
+	function Feed(slider){
+		this.makeSlides = (obj) => {
+			let slide = Object.assign(
+				document.createElement("div"),
+				{
+					style: `${slider.axis == "Y" ? "min-height": "min-width"}: 100%; position: relative;`
+				}
+			)
+			slide.appendChild(Object.assign(
+				document.createElement("img"),
+				{
+					src: obj.img,
+					style: "object-fit: cover; width: 100%; height: 100%; position: absolute"
+				}
+			));
+			slide.appendChild(Object.assign(
+				document.createElement("div"),
+				{
+					classList: ["slider-content"],
+					style: "width: 100%; position: absolute; bottom: 0",
+					innerHTML: [obj.content.title, obj.content.content].join("</br>")
+				}
+			));
+			return slide;
+		}
+		this.setTransformation = (slider) => {
+			return `translate${slider.axis}(${-(slider.axis == "Y" ? slider.html.clientHeight : slider.html.clientWidth) * slider.index}px)`
+		}
+		this.html = Object.assign(document.createElement("div"), {
+			classList: ["slider-feed"],
+			style: `
+				display: flex; 
+				height: 100%; 
+				flex-direction: ${slider.axis == "Y" ? "column" : "row"};`
+		});
+		this.html.style.transform = this.setTransformation(slider);
+		slider.data.forEach(d => {
+			console.log(slider, d);
+			this.html.appendChild(this.makeSlides(d));
+		})
+		this.html.addEventListener('transitionend', function () {
+			if (slider.index == slider.data.length - 1) {
+				this.html.style.transition = "none";
+				slider.index = 1;
+				this.html.style.transform = this.setTransformation(slider);
+			};
+			if (slider.index == 0) {
+				this.html.style.transition = "none";
+				slider.index = slider.data.length - 2;
+				this.html.style.transform = this.setTransformation(slider);
+			};
+		});
+		this.html.prepend("asldkjf;alskdjf;alksdjfa");
+		// console.log(this);
+		return this;
 	}
-	function animation(slider, startStop){
-		switch(startStop){
-			case"start":
-				slider.rotation = setInterval(function(){
+	
+// unused functions from v1...
+	function animation(slider, startStop) {
+		console.log(slider, startStop);
+		switch (startStop) {
+			case "start":
+				slider.rotation = setInterval(function () {
 					increment(slider, slider.direction);
 				}, slider.interval)
 				break;
@@ -50,78 +127,22 @@ window.sliderJSByMalikDunston = function(){
 				break;
 		}
 	}
-	function makeDataList(arr){
-		if (!arr) arr =  [
-			{
-				img: "",
-				content: {
-					title: "Your Slider Has No Data!",
-					content: "Attach some data to your slider element to begin."
-				}
-			}
-		];
-		return [arr[arr.length - 1], ...arr, arr[0]];
-	}
-	function setDefault(elem, name, def){
-		return elem.attributes.hasOwnProperty(name) ? elem.attributes[name].value : def
-	}
-	function render(slider){
-		if (!slider.html.style.height) slider.html.style.height = "500px";
-		slider.html.style.cssText += "position: relative; overflow: hidden; ";
-		slider.html.prepend(makeFeed(slider));
-	// should this go here?
-		function makeFeed(slider){
-			let feed = Object.assign(
-				document.createElement("div"),
-				{
-					classList: ["slider-feed"],
-					style: `display: flex; height: 100%; flex-direction: ${slider.axis == "Y" ? "column" : "row"}`
-				}
-			)
-		// same as set()
-			feed.style.transform = `translate${slider.axis}(${-(slider.axis == "Y" ? slider.html.clientHeight : slider.html.clientWidth) * slider.index}px)`;
-			slider.data.forEach(function(obj){
-				feed.appendChild(makeSlides(slider, obj));
-			})
-			feed.addEventListener('transitionend', function(){
-				if (slider.index == slider.data.length -1) {
-					feed.style.transition = "none";
-					slider.index = 1;
-					feed.style.transform = `translate${slider.axis}(${-(slider.axis == "Y" ? slider.html.clientHeight : slider.html.clientWidth) * slider.index}px)`;
-				};
-				if (slider.index == 0) {
-					feed.style.transition = "none";
-					slider.index = slider.data.length - 2;
-					feed.style.transform = `translate${slider.axis}(${-(slider.axis == "Y" ? slider.html.clientHeight : slider.html.clientWidth) * slider.index}px)`;
-				};
-			});
-			function makeSlides(slider, obj){
-				let slide = Object.assign(
-					document.createElement("div"),
-					{
-						style: `${slider.axis == "Y" ? "min-height": "min-width"}: 100%; position: relative;`
-					}
-				)
-				slide.appendChild(Object.assign(
-					document.createElement("img"),
-					{
-						src: obj.img,
-						style: "object-fit: cover; width: 100%; height: 100%; position: absolute"
-					}
-				));
-				slide.appendChild(Object.assign(
-					document.createElement("div"),
-					{
-						classList: ["slider-content"],
-						style: "width: 100%; position: absolute; bottom: 0",
-						innerHTML: [obj.content.title, obj.content.content].join("</br>")
-					}
-				));
-				return slide;
-			}
-			return feed;
+	function increment(slider, forBack) {
+		switch (forBack) {
+			case "forward":
+				if (slider.index >= slider.data.length - 1) return;
+				slider.index++;
+				break;
+			case "backward":
+				if (slider.index <= 0) return;
+				slider.index--;
+				break;
 		}
+		resetAnimation(slider);
 	}
-}
-window.addEventListener("load", sliderJSByMalikDunston);
-console.log("%cgithub.com/malikdunston", "color: yellow; font-style: italic; background-color: blue;padding: 20px 10px");
+	function resetAnimation(slider) {
+		animation(slider, "stop");
+		slider.html.querySelector(".slider-feed").style.transition = `${slider.transition}`;
+		slider.html.querySelector(".slider-feed").style.transform = setTransformation();
+		animation(slider, "start");
+	}
