96b2bd01a2b317672704b593f44b621e7f366b57
Malik Dunston <hello@malikdunston.com>
2021-08-27 21:12:11 -0400

    Controls()

--- slider.js
+++ slider.js
@@ -7,35 +7,33 @@
 	}
 // initialization...
 	window.addEventListener("load", function(event){
-		sliderJS.module = new Module(event);
-		window.addEventListener("resize", function () {
-		})
+		sliderJS.module = Module(event);
+		sliderJS.module.render();
+		// window.addEventListener("resize", function () {
+		// })
 		// console.log(sliderJS);
 	});
 // factories...
-	function Module(event){
-		this.idAttr = "slider-js";
-		this.scope = event.currentTarget;
-		this.html = event.target;
-		this.sliders = Array.from(this.html.querySelectorAll(`*[${this.idAttr}]`)).map(elem => {
-			return new Slider(elem, this);
-		})
-		this.interval = 1000;
-		this.counter = 0;
-		this.count = function () {
-			this.counter++;
+	const Module = event => {
+		this.scope = event.currentTarget, this.html = event.target;
+		this.interval = 1000, this.counter = 0;
+		this.render = () =>{
+			const childNodes = "sljs";
+			const count = () => this.counter++;
+			const sliders = Array.from(this.html.querySelectorAll(`*[${childNodes}]`)).map(elem => {
+				let slider = new Slider(elem, this);
+				slider.render();
+				return slider;
+			});
+			this.rotation ? clearInterval(this.rotation) : this.rotation = setInterval(count.bind(this), this.interval);
 		};
-		this.render = function (){
-			this.sliders.forEach(s=>s.renderSlider());
-			this.rotation ? clearInterval(this.rotation) : this.rotation = setInterval(this.count.bind(this), this.interval);
-		};
-		this.render();
+		return this
 	}
 	function Slider(elem, Module) {
-		this.setDefault = (elem, name, def) => {
+		const setDefault = (elem, name, def) => {
 			return elem.attributes.hasOwnProperty(name) ? elem.attributes[name].value : def
 		};
-		this.makeDataList = (arr) => {
+		const makeDataList = (arr) => {
 			if (!arr) arr = [{
 				img: "",
 				content: {
@@ -45,8 +43,7 @@
 			}];
 			return [arr[arr.length - 1], ...arr, arr[0]];
 		};
-	// Slide() 
-		this.makeSlides = (obj) => {
+		const makeSlides = (obj) => {
 			let slide = Object.assign(
 				document.createElement("div"),
 				{
@@ -85,25 +82,25 @@
 				this.feed.style.transform = `translate${this.axis}(${-(this.axis == "Y" ? this.html.clientHeight : this.html.clientWidth) * this.index}px)`;
 			};
 		}
-		this.increment = function(){
-			switch (this.direction) {
-				case "forward":
-					if (this.index >= this.data.length - 1) return;
-					this.index++;
-					break;
-				case "backward":
-					if (this.index <= 0) return;
-					this.index--;
-					break;
-			}
-			this.set();
-		}
 		this.animation = function(startStop){
+			const increment = () => {
+				switch (this.direction) {
+					case "forward":
+						if (this.index >= this.data.length - 1) return;
+						this.index++;
+						break;
+					case "backward":
+						if (this.index <= 0) return;
+						this.index--;
+						break;
+				}
+				this.set();
+			}
 			if (startStop == "start"){
-				setTimeout(this.increment.bind(this), this.interval)
+				this.rotation = setInterval(increment.bind(this), this.interval)
 				console.log(this.index, this.scope.length);
 			}else if (startStop == "stop"){
-				clearTimeout(this.rotation)
+				clearInterval(this.rotation)
 			}
 		}
 		this.set = function() {
@@ -113,33 +110,94 @@
 			this.html.querySelector(".slider-feed").style.transform = `translate${this.axis}(${-(this.axis == "Y" ? this.html.clientHeight : this.html.clientWidth) * this.index}px)`;
 			this.animation("start");
 		}
-		this.renderSlider = () => {
-			this.feed = Object.assign(document.createElement("div"), {
-				classList: ["slider-feed"],
-				style: `
-					display: flex; 
-					height: 100%; 
-					flex-direction: ${this.axis == "Y" ? "column" : "row"};
-					transform: ${this.setTransformation()};
-				`
-			}); // makeDataList should run in this...
-			this.data.forEach(d => {
-				this.feed.append(this.makeSlides(d));
-			})
-			this.html.prepend(this.feed);
-			this.feed.addEventListener('transitionend', (e)=>{this.loopFeed(e)});
-			this.html.style.cssText += "position: relative; overflow: hidden; ";
+		this.html = elem;
+		this.id = elem.attributes["sljs"].value;
+		this.scope = Module.scope[this.id];
+		this.data =  makeDataList(this.scope); // replace this w/this.slider.feed...
+		this.axis = setDefault(elem, "axis", "X");
+		this.index = parseInt(setDefault(elem, "offset", "1"));
+		this.interval = setDefault(elem, "interval", "4000");
+		this.direction = setDefault(elem, "direction", "forward");
+		this.transition = setDefault(elem, "transition", "100ms");
+		this.controls = setDefault(elem, "controls", "1");
+		this.delay = setDefault(elem, "delay", "1");
+		this.feed = Object.assign(document.createElement("div"), {
+			classList: ["slider-feed"],
+			style: `
+				display: flex; 
+				height: 100%; 
+				flex-direction: ${this.axis == "Y" ? "column" : "row"};
+				transform: ${this.setTransformation()};
+			`,
+			innerHTML: this.data.map(d => makeSlides(d).outerHTML).join(""),
+			ontransitionend: (e)=>{this.loopFeed(e)}
+		});
+		this.html = Object.assign(elem, {
+			style: `
+				height: 325px; 
+				position: relative;
+				overflow: hidden;
+			`
+		});
+		this.render = () => {
+			let controls = Controls();
+			console.log(controls);
+			this.html.append(
+				controls,
+				this.feed,
+			);
 			this.animation("start");
 		}
-		this.html = elem;
-		this.id = elem.attributes["slider-js"].value;
-		this.axis = this.setDefault(elem, "axis", "X");
-		this.index = parseInt(this.setDefault(elem, "offset", "1"));
-		this.interval = this.setDefault(elem, "interval", "4000");
-		this.direction = this.setDefault(elem, "direction", "forward");
-		this.transition = this.setDefault(elem, "transition", "100ms");
-		this.delay = this.setDefault(elem, "delay", "1");
-		this.scope = Module.scope[this.id];
-		this.data =  this.makeDataList(this.scope); // replace this w/this.slider.feed...
 		return this;
+	}
+
+	const Controls = () => {
+		const viewBox = "0 0 100 100";
+		const btnStyles = `
+			width: 2rem;
+			height: 2rem;
+		`;
+		this.backward = Object.assign(document.createElement("svg"), {
+			style: btnStyles,
+			title: "backward",
+			innerHTML: `<polyline points="100 0 50 50 100 100" />`,
+			onclick: () => {
+				alert("bac");
+			}
+		});
+		this.forward = Object.assign(document.createElement("svg"), {
+			style: btnStyles,
+			title: "forward",
+			innerHTML: `<polyline points="0 100 50 50 0 0" style="pointer-events: none"/>`,
+		});
+		this.backward.setAttribute("viewbox", viewBox)
+		this.forward.setAttribute("viewbox", viewBox)
+		this.html = Object.assign(document.createElement("div"), {
+			classList: ["slider-controls"],
+			style: `
+				fill: none;
+				stroke: white;
+				stroke-miterlimit: 10;
+				stroke-width: 10px;
+				position: absolute;
+				z-index: 500;
+				width: 100%;				
+				height: 100%;		
+				display: flex;
+				align-items: center;
+				justify-content: space-between;	
+			`,
+			innerHTML: [this.backward.outerHTML, this.forward.outerHTML].join(""),
+			onclick: e => {
+				let x;
+				if(e.target.nodeName == "svg") {
+					e.target.title == "backward" ? x = "bac" : x = "for"
+					console.dir(e.target)
+				}
+			}
+		});
+		// this.html.addEventListener("click", function(e){
+		// 	console.dir(e.target);
+		// })
+		return this.html;
 	}
