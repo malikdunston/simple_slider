eb8616d04f724b057491686246d385a9017fa141
Malik Dunston <hello@malikdunston.com>
2021-11-13 11:47:29 -0500

    new package

--- src/Components/Slider/Feed/Slide.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Slide({ slide }) {
-	return <div className="slide" style={{
-		position: "relative",
-		[slide.axis === "Y" ? "minHeight" : "minWidth"]: "100%"
-	}}>
-		{slide.template}
-	</div>
-}
--- src/Components/Slider/XScroll/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Card({ card, currentIndex  }) {
-	return <div className={ "card" + (card.index === currentIndex ? " selected" : "") }style={{
-		[card.axis === "Y" ? "min-height" : "min-width"]: card.cardSize,
-		[card.axis === "Y" ? "min-width" : "min-height"]: "100%"
-	}} >
-		{card.template}
-	</div>
-}
