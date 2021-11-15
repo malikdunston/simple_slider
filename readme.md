270b9a6d9d08d2f1e925f490931dafa0b6d9e50d
Malik Dunston <hello@malikdunston.com>
2021-11-15 12:33:13 -0500

    empty... npm pkg works

--- readme.md
+++
@@ -1,4 +0,0 @@
-## Sources
-
-### Youtube
-- (Build and Publish a React Component Library)[https://www.youtube.com/watch?v=hf6Z8OZanec]
--- src/Components/Requirements/Feed/Slide.js
+++
@@ -1,8 +0,0 @@
-export default function Slide({ slide }) {
-	return <div className="slide" style={{
-		position: "relative",
-		[slide.axis === "Y" ? "minHeight" : "minWidth"]: "100%"
-	}}>
-		{slide.template}
-	</div>
-}
